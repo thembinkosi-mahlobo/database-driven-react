c82a52ca928ee11f01f70ecf45649105f4a059c7
Thembinkosi Mahlobo <themba.romano@hotmail.co.uk>
2024-05-04 22:44:20 +0100

    add all changes made to client and server

--- database-driven-project/client/src/StudentForm.jsx
+++ database-driven-project/client/src/StudentForm.jsx
@@ -1,31 +1,57 @@
-import React from "react";
 import { useState } from "react";
 
-export default function StudentForm(){
-    constant [form,setForm] = useState({name:"",email:"", age:0});
+export default function StudentForm() {
+  const [form, setForm] = useState({ name: "", email: "", age: 0 });
 
-    function handleSubmit(event){
-        event.preventDefault();
-        fetch("http://localhost:8080/students",{
-            methode: "POST",
-            body:JSON.stringify(form),
-            headers: {
-                "Content-Type":"application/json",
-            },
-        });
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("http://localhost:8080/students", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-    function handleChange(event) {
-        setForm({ ...form, [event.target.name]: event.target.value});
-    }
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  return (
     <form>
-        <h3> Add a new student</h3>
-        <p> Having a hobby or doing activities in you spare time is great for you MH</p>
-        <input name="name" placeholder="Name" onChange={handleChange}/>
-        <input name="email" type="email" placeholder="address"  onChange={handleChange}/>
-        <input name="age" type="number" placeholder="Age" onChange={handleChange} />
-        <button>Add Student</button>
-        </form>
-};
+      <h3>Add a new student</h3>
+      <p>
+        Having a hobby or doing activities in your spare time is great for your
+        MH.
+      </p>
+      <label>
+        UserName:
+        <input name="name" placeholder="Name" onChange={handleChange} />
+      </label>
+      <label>
+        Email:
+        <input
+          name="email"
+          type="email"
+          placeholder="address"
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Age:
+        <input
+          name="age"
+          type="number"
+          placeholder="Age"
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        <input type="submit" placeholder="Submit" onChange={handleSubmit} />
+      </label>
+      <button>Add Student</button>
+      <button>Insert Hobby</button>
+    </form>
+  );
 }
-
